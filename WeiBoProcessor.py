36423d9b250ca08c639862f0eb13fcb2bef97ff8
Eli Qiao <taget@linux.vnet.ibm.com>
2012-06-05 10:06:13 +0800

    v2

---
+++ WeiBoProcessor.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+process command line  v2
+'''
+import argparse
+import urllib2
+import urllib
+import sys
+import re
+import base64
+import logging
+from urlparse import urlparse 
+from Interface import WeiboInterface
+from Json import JsonProcessor
+
+class WeiboProcessor:
+	def  __init__(self, username, passwd):
+		self.parser = argparse.ArgumentParser()
+		self.parser.add
+	def process(self, cmd):
+		'''
+			process cmd line
+		'''
+		
+		
+	
