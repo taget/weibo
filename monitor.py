ed97674339de5c4193c48b31ba98faaa665410c8
Eli Qiao <taget@linux.vnet.ibm.com>
2013-03-04 15:19:27 +0800

    add log

--- monitor.py
+++ monitor.py
@@ -49,29 +49,49 @@ class Monitor:
 		except:
 			print "error happend when read conf"
 			sys.exit(1)
+
 		self._interface = WeiboInterface(self._username, self._passwd)
-		self._logger = logging.getLogger('Monitor.log')
+		self._logger = logging.getLogger()
+
+		hdlr = logging.FileHandler('Monitor.log')
+		formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+		hdlr.setFormatter(formatter)
+		self._logger.addHandler(hdlr)
+		self._logger.setLevel(logging.NOTSET)
+
 		self._lastmsg = ''
+		self._sleep_time = 10
 		self._run = False
-		
+
+	def info(self, msg):
+		self._logger.info(msg)
+
+	def error(self, msg):
+		self._logger.error(msg)
+
 	def run_command(self, cmd):
 		print "ready to run shell command: " + cmd
+		self.info("ready to run shell command: " + cmd)
 		rc = -1
 		out = ''
 		err = ''
 		rc, out, err = exec_command(cmd)
 		if rc == 0:
-			ret_msg = 'done ' + cmd
-			return 1
+			return "".join(out.split());
 		else:
 			return 0
-			
+
 	def parse_keyword(self, msg):
-		return msg[7:11] # find exec 
-		
+		return msg[7:11] # find exec
+
 	def parse_cmd(self, msg): # find shell command
 		return msg[12:]
-			
+
+	def run(self):
+		while True:
+			mon.loop()
+			time.sleep(self._sleep_time)
+
 	def loop(self):
 		self._interface.seturl('statuses/mentions')
 		self._interface.addopt('count','2')
@@ -80,7 +100,6 @@ class Monitor:
 		msg = msg_list.get_message(0)
 		usr = msg.msg_user()
 		ret_msg = msg.msg_text()
-		
 		
 		if self._lastmsg != ret_msg:
 			self._lastmsg = ret_msg
@@ -93,22 +112,28 @@ class Monitor:
 		
 		if self._run == True and key_word == self._keyword:
 			cmd = self.parse_cmd(ret_msg)
-			if self.run_command(cmd):
-				ret = 'done ' + cmd
+			res = self.run_command(cmd)
+
+			if res != 0:
+				ret = 'done result is:'+ res
 				data = writetext(ret)
+				print data
 				try:
 					self._interface.seturl('statuses/update')
 					self._interface.callweibo(data)
 					print 'run successful , write back to weibo'
+					self.info("run successful , write back to weibo")
 				except:
 					print "wrong"
+					self.error("return msg to weibo error")
+			else:
+				# run failed
+				self._lastmsg = ''
 
 def main():
 	mon = Monitor()
-	while True:
-		mon.loop()
-		
-		time.sleep(5)
+	mon.run()
+
 		
 
 if __name__ == '__main__':
